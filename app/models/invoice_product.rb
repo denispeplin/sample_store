415a47032fd0dec56301c33316b11bc4404587a5
denispeplin <denis.peplin@gmail.com>
2014-11-23 16:33:16 +0300

    Validate invoice_products.

--- app/models/invoice_product.rb
+++ app/models/invoice_product.rb
@@ -1,3 +1,8 @@
 class InvoiceProduct < ActiveRecord::Base
   belongs_to :invoice
+
+  validates :name, presence: true
+  validates :amount, numericality: {only_integer: true,
+    greater_than_or_equal_to: 1}
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
