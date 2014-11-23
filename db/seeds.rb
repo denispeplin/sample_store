a441984f11ac3f109ea9c416a461137cc0037539
denispeplin <denis.peplin@gmail.com>
2014-11-23 16:33:59 +0300

    Add faker gem, seed invoces and seed invoice_products.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+invoice = Invoice.create!
+
+20.times do
+  invoice.invoice_products.create!(
+    name: Faker::Commerce::product_name,
+    amount: Faker::Number.number(2).to_i + 5,
+    price: Faker::Commerce::price
+  )
+end
