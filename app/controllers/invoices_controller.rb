bcbfd9324257a74d6ed74e2e580acf0fccf70212
denispeplin <denis.peplin@gmail.com>
2014-11-24 19:41:32 +0300

    Add 'update' action to InvoicesController.

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,4 +2,19 @@ class InvoicesController < ApplicationController
   def index
     render json: Invoice.all
   end
+
+  def update
+    @invoice = Invoice.find params[:id]
+    if @invoice.update_attributes(invoice_params)
+      render json: @invoice
+    else
+      render json: @invoice.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def invoice_params
+    params.require(:invoice).permit(:received)
+  end
 end
