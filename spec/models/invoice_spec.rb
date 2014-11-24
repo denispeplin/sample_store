7bd56a5f13a3bbd5c559cc1adfaad84e734382e9
Denis Peplin <denis.peplin@gmail.com>
2014-11-24 15:12:11 +0300

    Move received/not_received test appropriate contexts.

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -2,17 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Invoice, :type => :model do
   describe 'may become received' do
-    it "does nothing when already received" do
-      invoice = create :invoice, received: true
-      updated_at = invoice.updated_at
-      invoice.receive!
-      expect(invoice.updated_at).to eq updated_at
+    context "when already received" do
+      it "doesn't mark itself as received" do
+        invoice = create :invoice, received: true
+        updated_at = invoice.updated_at
+        invoice.receive!
+        expect(invoice.updated_at).to eq updated_at
+      end
     end
 
-    it 'marks itself as received' do
-      invoice = create :invoice
-      invoice.receive!
-      expect(invoice).to be_received
+    context "when not received" do
+      it 'marks itself as received' do
+        invoice = create :invoice
+        invoice.receive!
+        expect(invoice).to be_received
+      end
     end
   end
 end
