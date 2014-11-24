2d1433c10009a0e89f19fa5a59f5583d99352692
Denis Peplin <denis.peplin@gmail.com>
2014-11-24 14:23:45 +0300

    Impement InvoiceProduct copy to Product.

--- app/models/invoice_product.rb
+++ app/models/invoice_product.rb
@@ -4,4 +4,11 @@ class InvoiceProduct < ActiveRecord::Base
 
   validates :amount, numericality: {only_integer: true,
     greater_than_or_equal_to: 1}
+
+  def copy
+    product = Product.find_by_name(name) ||
+      Product.new(attributes.extract!('name', 'amount', 'price'))
+    product.amount += amount unless product.new_record?
+    product.save!
+  end
 end
--- spec/models/invoice_product_spec.rb
+++ spec/models/invoice_product_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceProduct, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'may be copied to Product' do
+    before :each do
+      @invoice_product = create :invoice_product
+      @invoice_product.copy
+    end
+
+    it "creates new, when product with the same name doesn't exists" do
+      @invoice_product.attributes.extract!('name', 'amount', 'price').
+        should == Product.last.attributes.extract!('name', 'amount', 'price')
+    end
+
+    it "increases amount, when product with the same name does exists" do
+      @invoice_product.copy
+      Product.last.amount.should == @invoice_product.amount * 2
+    end
+  end
 end
