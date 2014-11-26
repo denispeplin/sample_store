785cfd994de312b30477bc14bc1686d5f63bd9df
denispeplin <denis.peplin@gmail.com>
2014-11-26 21:53:11 +0300

    Implement bid placing. Only mark them placed for now.

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -2,4 +2,20 @@ class BidsController < ApplicationController
   def index
     render json: Bid.all
   end
+
+  def update
+    @bid = Bid.find params[:id]
+
+    if @bid.update_attributes(bid_params)
+      render json: @bid
+    else
+      render json: @bid.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bid_params
+    params.require(:bid).permit(:placed)
+  end
 end
