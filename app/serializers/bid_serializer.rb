e11c8a44db2bda3b1d59c186574b0beef057f18f
Denis Peplin <denis.peplin@gmail.com>
2014-11-27 09:40:16 +0300

    Add virtual attribute 'combine' to bid.

--- app/serializers/bid_serializer.rb
+++ app/serializers/bid_serializer.rb
@@ -1,6 +1,10 @@
 class BidSerializer < ActiveModel::Serializer
   embed :ids, include: true
 
-  attributes :id, :placed
+  attributes :id, :placed, :combine
   has_many :bid_products
+
+  def combine
+    []
+  end
 end
